acc8b338482f498d6f06ec65f8e0bc7563ae926d
anyushu <suzuki@lamp.jp>
2021-12-16 16:26:29 +0900

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,19 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import React from 'react'
+import {
+  useGenerationsList,
+  useGeneration,
+  useRegionsList,
+  useRegion,
+  usePokemonsList,
+  usePokemon,
+} from 'lib/pokeapi'
 
 const Home: NextPage = () => {
+  const { pokemon, isLoading, isError } = usePokemon(1)
+  console.log(pokemon)
+
   return (
     <>
       <Head>
