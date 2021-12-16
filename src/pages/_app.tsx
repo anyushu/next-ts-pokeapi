db57a1a9f20299787e9e1c95a1c8d17747a02d98
anyushu <suzuki@lamp.jp>
2021-12-16 16:16:46 +0900

    add swr global conf

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,20 @@
 import 'styles/globals.css'
 import { DefaultSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
+import { SWRConfig } from 'swr'
+import { fetcher } from 'lib/pokeapi'
 import { defaultSeo } from 'next-seo.config'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <SWRConfig
+      value={{
+        fetcher: fetcher,
+      }}
+    >
       <DefaultSeo {...defaultSeo} />
       <Component {...pageProps} />
-    </>
+    </SWRConfig>
   )
 }
 
